82015b75dddb8059697946c3a0b00e3088ba3c2f
Dani <1073B023@gmail.com>
2024-02-23 16:44:28 +0800

    update css and html

---
+++ 00Project/ShowNest_MVC/ShowNest-MVC/ShowNest-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShowNest_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
